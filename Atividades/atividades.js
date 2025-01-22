6e53c1c2745096564cd47d01d0812470005eee25
guirossbach <guilhermerossbach12@gmail.com>
2025-01-21 21:29:11 -0300

    aula 3

--- Atividades/atividades.js
+++ Atividades/atividades.js
@@ -1,3 +1,5 @@
+// atividade 2 - mensagem de boas vindas
+
 alert ('Bem vindo a minha página!');
 
 function exibirMensagem () {
@@ -78,4 +80,72 @@ function calcular(){
  
     alert('resultado: ' +resultado);
     limpar();
+}
+// atividade 5 - verificar o sono
+
+function verificarSono(){
+    const qtdHoras = prompt('informar a quantidade de horas que você dorme em média:');
+    const qntdMinimaHorasSono = 8;
+    if(qtdHoras<qntdMinimaHorasSono){
+        alert('Você esta dormindo muito pouco.')
+    } else {
+        alert ('Você está dormindo o suficiente. Parabéns!')
+    }
+}
+
+// atividade 6 - relógio 12h
+
+function relogioAmPm(){
+   const horaDesejada = prompt('Digite uma hora do dia do relógio 24h:');
+   const manha ='a.m';
+   const tarde = 'p.m';
+
+   if(horaDesejada<=12){
+    alert(horaDesejada+ manha );
+   } else{
+    alert(horaDesejada-12+ tarde);
+   }
+}
+ // atividade 5 - operadores E e OU 
+
+function testarOperadorOU(){
+    const nmr = prompt ('informe um digito:');
+    if(nmr == 5 || nmr == 10){
+        alert('O número é 5 ou 10');
+    }else{
+        alert('Outro número digitado que não é 5 ou 10');
+    }  
+}
+
+function testarOperadorE(){
+    const idade = prompt ('informe a sua idade:');
+    const idadeAmigo = prompt ('informe a idade do seu amigo:');
+    if(idade>=18 && idadeAmigo >=18){
+        alert('pode bebe pra caralho');
+    } else{
+        alert('Não pode bebe ainda');
+    }
+}
+
+function verificarEntradaBalada() {
+    const idade = prompt ('informe a sua idade');
+    const lista = prompt ('Você tem nome na lista? Digite sim ou não');
+    const ingresso = prompt('Você tem ingresso? Digite sim ou não.');
+    
+    if( idade > 18 && (ingresso == 'sim' || lista == 'sim') ){
+        alert('Entrata permtida! Boa Festa');
+    } else {
+        alert('Entrada negada.');
+    }
+}
+
+//atividade 8 - direito a meia entrada
+
+function meiaEntratada(){
+    const idade = prompt ('Informe a sua idade:')
+    if(idade <=12 || idade > 60){
+        alert ('Você tem direito a meia-entrada.')
+    } else{
+        alert('Você não tem direito a meia-entrada.')
+    }
 }
