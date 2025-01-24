db6cc92037666819cb27898f1df9d43ad0b0c9eb
guirossbach <guilhermerossbach12@gmail.com>
2025-01-23 21:20:16 -0300

    aula 4

--- Atividades/atividades.js
+++ Atividades/atividades.js
@@ -1,8 +1,8 @@
 // atividade 2 - mensagem de boas vindas
 
-alert ('Bem vindo a minha página!');
+alert('Bem vindo a minha página!');
 
-function exibirMensagem () {
+function exibirMensagem() {
     alert('Você clicou no botão');
     console.log('A página executou o metado');
 }
@@ -20,119 +20,119 @@ function realizarCalculos() {
 
 function somar(valor1, valor2) {
     let resultado = valor1 + valor2;
-    console.log('O resultado da soma entre '+valor1+' e '+valor2+' é: '+resultado);
+    console.log('O resultado da soma entre ' + valor1 + ' e ' + valor2 + ' é: ' + resultado);
 }
 
 function subtratir(valor1, valor2) {
-    let resultado =valor1 - valor2;
-    console.log('O resultado da subtração entre '+valor1+' e '+valor2+' é: '+resultado);
+    let resultado = valor1 - valor2;
+    console.log('O resultado da subtração entre ' + valor1 + ' e ' + valor2 + ' é: ' + resultado);
 }
 
 function dividir(valor1, valor2) {
     let resultado = valor1 / valor2;
-    console.log('O resultado da divisão entre '+valor1+' e '+valor2+' é: '+resultado);
+    console.log('O resultado da divisão entre ' + valor1 + ' e ' + valor2 + ' é: ' + resultado);
 }
 
 function multiplicar(valor1, valor2) {
-    let resultado =valor1 * valor2;
-    console.log('O resultado da multiplicação entre '+valor1+' e '+valor2+' é: '+resultado);
+    let resultado = valor1 * valor2;
+    console.log('O resultado da multiplicação entre ' + valor1 + ' e ' + valor2 + ' é: ' + resultado);
 }
 
 //Calculadora "funcional"
-var valor1='';
-var valor2='';
-var operacao='';
+var valor1 = '';
+var valor2 = '';
+var operacao = '';
 
-function guardarNumero(valor){
-    if(!operacao){
+function guardarNumero(valor) {
+    if (!operacao) {
         valor1 += valor;
         console.log(valor1);
     }
     //se a operacao for informada
-    if (operacao){
+    if (operacao) {
         valor2 += valor;
         console.log(valor2);
     }
 }
 
-function guardarOperacao(operacaoE){
+function guardarOperacao(operacaoE) {
     operacao = operacaoE
 }
 
-function limpar(){
-    valor1='';
-    valor2='';
-    operacao='';
+function limpar() {
+    valor1 = '';
+    valor2 = '';
+    operacao = '';
 }
 
-function calcular(){
+function calcular() {
     valor1 = parseFloat(valor1);
     valor2 = parseFloat(valor2);
-    if (operacao == '+'){
-        resultado=valor1+valor2;
-    } else if(operacao == '-'){
-        resultado=valor1-valor2;
-    } else if(operacao == '*'){
-        resultado=valor1*valor2;
-    } else if(operacao == '/'){
-        resultado=valor1/valor2;
+    if (operacao == '+') {
+        resultado = valor1 + valor2;
+    } else if (operacao == '-') {
+        resultado = valor1 - valor2;
+    } else if (operacao == '*') {
+        resultado = valor1 * valor2;
+    } else if (operacao == '/') {
+        resultado = valor1 / valor2;
     }
- 
-    alert('resultado: ' +resultado);
+
+    alert('resultado: ' + resultado);
     limpar();
 }
 // atividade 5 - verificar o sono
 
-function verificarSono(){
+function verificarSono() {
     const qtdHoras = prompt('informar a quantidade de horas que você dorme em média:');
     const qntdMinimaHorasSono = 8;
-    if(qtdHoras<qntdMinimaHorasSono){
+    if (qtdHoras < qntdMinimaHorasSono) {
         alert('Você esta dormindo muito pouco.')
     } else {
-        alert ('Você está dormindo o suficiente. Parabéns!')
+        alert('Você está dormindo o suficiente. Parabéns!')
     }
 }
 
 // atividade 6 - relógio 12h
 
-function relogioAmPm(){
-   const horaDesejada = prompt('Digite uma hora do dia do relógio 24h:');
-   const manha ='a.m';
-   const tarde = 'p.m';
+function relogioAmPm() {
+    const horaDesejada = prompt('Digite uma hora do dia do relógio 24h:');
+    const manha = 'a.m';
+    const tarde = 'p.m';
 
-   if(horaDesejada<=12){
-    alert(horaDesejada+ manha );
-   } else{
-    alert(horaDesejada-12+ tarde);
-   }
+    if (horaDesejada <= 12) {
+        alert(horaDesejada + manha);
+    } else {
+        alert(horaDesejada - 12 + tarde);
+    }
 }
- // atividade 5 - operadores E e OU 
+// atividade 5 - operadores E e OU 
 
-function testarOperadorOU(){
-    const nmr = prompt ('informe um digito:');
-    if(nmr == 5 || nmr == 10){
+function testarOperadorOU() {
+    const nmr = prompt('informe um digito:');
+    if (nmr == 5 || nmr == 10) {
         alert('O número é 5 ou 10');
-    }else{
+    } else {
         alert('Outro número digitado que não é 5 ou 10');
-    }  
+    }
 }
 
-function testarOperadorE(){
-    const idade = prompt ('informe a sua idade:');
-    const idadeAmigo = prompt ('informe a idade do seu amigo:');
-    if(idade>=18 && idadeAmigo >=18){
+function testarOperadorE() {
+    const idade = prompt('informe a sua idade:');
+    const idadeAmigo = prompt('informe a idade do seu amigo:');
+    if (idade >= 18 && idadeAmigo >= 18) {
         alert('pode bebe pra caralho');
-    } else{
+    } else {
         alert('Não pode bebe ainda');
     }
 }
 
 function verificarEntradaBalada() {
-    const idade = prompt ('informe a sua idade');
-    const lista = prompt ('Você tem nome na lista? Digite sim ou não');
+    const idade = prompt('informe a sua idade');
+    const lista = prompt('Você tem nome na lista? Digite sim ou não');
     const ingresso = prompt('Você tem ingresso? Digite sim ou não.');
-    
-    if( idade > 18 && (ingresso == 'sim' || lista == 'sim') ){
+
+    if (idade > 18 && (ingresso == 'sim' || lista == 'sim')) {
         alert('Entrata permtida! Boa Festa');
     } else {
         alert('Entrada negada.');
@@ -141,11 +141,100 @@ function verificarEntradaBalada() {
 
 //atividade 8 - direito a meia entrada
 
-function meiaEntratada(){
-    const idade = prompt ('Informe a sua idade:')
-    if(idade <=12 || idade > 60){
-        alert ('Você tem direito a meia-entrada.')
-    } else{
+function meiaEntratada() {
+    const idade = prompt('Informe a sua idade:')
+    if (idade <= 12 || idade > 60) {
+        alert('Você tem direito a meia-entrada.')
+    } else {
         alert('Você não tem direito a meia-entrada.')
     }
 }
+
+// atividade 9 - nome na lista
+function identificarNomeUsuario() {
+    const nomeUsuario = prompt('informe seu nome');
+    if (!nomeUsuario) {
+        alert('Entrada esta proibida. Obrigada por informar seu nome');
+        identificarNomeUsuario();
+    } else {
+        alert('Entrada liberada!')
+    }
+}
+
+
+function calcularComSwitch() {
+    valor1 = parseFloat(valor1);
+    valor2 = parseFloat(valor2);
+    let resultado;
+    switch (operacao) {
+        case '+':
+            resultado = valor1 + valor2;
+            break;
+        case '-':
+            resultado = valor1 - valor2;
+            break;
+        case '*':
+            resultado = valor1 * valor2;
+            break;
+        case '/':
+            resultado = valor1 / valor2;
+            break;
+
+    }
+    alert('resultado: ' + resultado);
+    limpar();
+}
+
+function IMC() {
+    const peso = prompt('informe seu peso');
+    const altura = prompt('informe sua altura');
+    const IMC = peso / (altura * altura);
+
+    if (IMC <= 18.50) {
+        alert('Você esta abaixo do peso');
+    } else if (IMC > 18.5 && IMC < 25) {
+        alert('Você esta no peso ideal');
+    } else if (IMC > 25 && IMC < 30) {
+        alert('Você esta acima do peso');
+    } else if (IMC > 30) {
+        alert('Você esta obeso');
+    }
+
+}
+
+function converterDiasDaSemana(){
+    const numero =  parseInt (prompt ('Digite um número'));
+    const DOMINGO = 1, SEGUNDA=2, TERCA = 3, QUARTA=4, QUINTA = 5, SEXTA = 6, SABADO = 7;
+
+    switch (numero) {
+        case DOMINGO :
+            resultado = alert('DOMINGO');
+            break;
+        case SEGUNDA :
+            resultado = alert('SEGUNDA');
+            break;           
+        case TERCA:
+            resultado = alert('TERCA');
+            break;
+        case QUARTA:
+            resultado = alert('QUARTA');
+            break;
+        case QUINTA:
+            resultado = alert('QUINTA');
+            break;
+        case SEXTA:
+            resultado = alert('SEXTA');
+            break;
+        case SABADO:
+            resultado = alert('SABADO');
+            break;
+        default:{
+            resultado = alert('DIA INVÁLIDO');
+        }
+    }
+}
+
+function paginaCobranca(){
+    const
+}
+
